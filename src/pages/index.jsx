b83a28fac5f5f533be9d200967e6c4863d9fe906
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2021-10-28 18:17:46 -0300

    feat: add background-animation

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,12 +5,14 @@ import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
 import Timeline from "../components/TimeLine/TimeLine";
 import Acomplishments from "../components/Acomplishments/Acomplishments";
+import BgAnimation from "../components/BackgroundAnimation/BackgroundAnimation";
 
 const Home = () => {
   return (
     <Layout>
       <Section grid>
         <Hero />
+        <BgAnimation />
       </Section>
       <Projects />
       <Technologies />
