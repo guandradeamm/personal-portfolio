f3b2c045ce029cfa527b1011dc4278d0ef26f0f3
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2021-10-28 17:50:02 -0300

    feat: add timeline section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import { Layout } from "../layout/Layout";
 import { Section } from "../styles/GlobalComponents";
 import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
+import Timeline from "../components/TimeLine/TimeLine";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       </Section>
       <Projects />
       <Technologies />
+      <Timeline />
     </Layout>
   );
 };
